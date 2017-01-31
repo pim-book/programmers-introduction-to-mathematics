e1dcbf995f7abeeea7c1f4a4965647f002ce701f
Jeremy Kun <kun.jeremy@gmail.com>
2017-01-31 07:14:34 -0800

    planar-5-coloring: pep8

--- planar-5-coloring/unittest.py
+++ planar-5-coloring/unittest.py
@@ -1,8 +1,9 @@
 def test(expected, actual):
-   if expected != actual:
-      import sys, traceback
-      (filename, lineno, container, code) = traceback.extract_stack()[-2]
-      print("Test: %r failed on line %d in file %r.\nExpected %r but got %r\n" %
-         (code, lineno, filename, expected, actual))
+    if expected != actual:
+        import sys
+        import traceback
+        (filename, lineno, container, code) = traceback.extract_stack()[-2]
+        print("Test: %r failed on line %d in file %r.\nExpected %r but got %r\n" %
+              (code, lineno, filename, expected, actual))
 
-      sys.exit(1)
+        sys.exit(1)
