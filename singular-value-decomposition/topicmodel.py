f7828c0687d36599cb065f6fa037880dc6358125
Jeremy Kun <j2kun@users.noreply.github.com>
2018-09-12 09:20:49 -0700

    singular-value-decomposition: python style

--- singular-value-decomposition/topicmodel.py
+++ singular-value-decomposition/topicmodel.py
@@ -12,36 +12,36 @@ from svd import svd
 
 
 def normalize(matrix):
+    '''Normalize a document term matrix according to a local and
+    global normalization factor.
+
+    For this we chose a simple logarithmic local normalization
+    with a global normalization based on entropy.
     '''
-        Normalize a document term matrix according to a local
-        and global normalization factor. For this we chose a
-        simple logarithmic local normalization with a global
-        normalization based on entropy.
-    '''
-    numWords, numDocs = matrix.shape
-    localFactors = np.log(np.ones(matrix.shape) + matrix.copy())
+    num_words, num_docs = matrix.shape
+    local_factors = np.log(np.ones(matrix.shape) + matrix.copy())
 
     probabilities = matrix.copy()
-    rowSums = np.sum(matrix, axis=1)
+    row_sums = np.sum(matrix, axis=1)
 
     # divide each column by the row sums
-    assert all(x > 0 for x in rowSums)
-    probabilities = (probabilities.T / rowSums).T
+    assert all(x > 0 for x in row_sums)
+    probabilities = (probabilities.T / row_sums).T
 
     entropies = (probabilities * np.ma.log(probabilities).filled(0) /
-                 np.log(numDocs))
-    globalFactors = np.ones(numWords) + np.sum(entropies, axis=1)
+                 np.log(num_docs))
+    global_factors = np.ones(num_words) + np.sum(entropies, axis=1)
 
     # multiply each column by the global factors for the rows
-    normalizedMatrix = (localFactors.T * globalFactors).T
-    return normalizedMatrix
+    normalized_matrix = (local_factors.T * global_factors).T
+    return normalized_matrix
 
 
-def makeDocumentTermMatrix(data):
-    '''
-        Return the document-term matrix for the given list of stories.
-        stories is a list of dictionaries {string: string|[string]}
-        of the form
+def make_document_term_matrix(documents):
+    '''Return the document-term matrix for the given list of stories.
+
+    Arguments:
+        documents: a list of dictionaries of the form
 
             {
                 'filename': string
@@ -49,59 +49,66 @@ def makeDocumentTermMatrix(data):
                 'text': string
             }
 
-        The list of words include repetition, and the output document-
-        term matrix contains as entry [i,j] the count of word i in story j
+        The list of words include repetition.
+
+    Returns:
+        A document-term matrix. Entry [i, j] is the count of word i
+        in story j.
     '''
-    words = allWords(data)
-    wordToIndex = dict((word, i) for i, word in enumerate(words))
-    indexToWord = dict(enumerate(words))
-    indexToDocument = dict(enumerate(data))
+    words = all_words(documents)
+    word_to_index = dict((word, i) for i, word in enumerate(words))
+    index_to_word = dict(enumerate(words))
+    index_to_document = dict(enumerate(documents))
 
-    matrix = np.zeros((len(words), len(data)))
-    for docID, document in enumerate(data):
-        docWords = Counter(document['words'])
-        for word, count in docWords.items():
-            matrix[wordToIndex[word], docID] = count
+    matrix = np.zeros((len(words), len(documents)))
+    for doc_id, document in enumerate(documents):
+        doc_words = Counter(document['words'])
+        for word, count in doc_words.items():
+            matrix[word_to_index[word], doc_id] = count
 
-    return matrix, (indexToWord, indexToDocument)
+    return matrix, (index_to_word, index_to_document)
 
 
 def cluster(vectors):
     return kmeans2(vectors, k=len(vectors[0]))
 
 
-def allWords(data):
+def all_words(documents):
+    '''Return a list of all unique words in the input list of documents.'''
     words = set()
-    for entry in data:
+    for entry in documents:
         words |= set(entry['words'])
     return list(sorted(words))
 
 
 def load():
     with open('all_stories.json', 'r') as infile:
-        data = json.loads(infile.read())
-    return data
+        return json.loads(infile.read())
 
 
-if __name__ == "__main__":
-    data = load()
-    matrix, (indexToWord, indexToDocument) = makeDocumentTermMatrix(data)
+def cluster_stories(documents):
+    matrix, (index_to_word, index_to_document) = make_document_term_matrix(documents)
     matrix = normalize(matrix)
     sigma, U, V = svd(matrix, k=10)
 
-    projectedDocuments = np.dot(matrix.T, U)
-    projectedWords = np.dot(matrix, V.T)
+    projected_documents = np.dot(matrix.T, U)
+    projected_words = np.dot(matrix, V.T)
 
-    documentCenters, documentClustering = cluster(projectedDocuments)
-    wordCenters, wordClustering = cluster(projectedWords)
+    document_centers, document_clustering = cluster(projected_documents)
+    word_centers, word_clustering = cluster(projected_words)
 
-    wordClusters = [
-        [indexToWord[i] for (i, x) in enumerate(wordClustering) if x == j]
+    word_clusters = [
+        [index_to_word[i] for (i, x) in enumerate(wordClustering) if x == j]
         for j in range(len(set(wordClustering)))
     ]
 
-    documentClusters = [
-        [indexToDocument[i]['text']
-         for (i, x) in enumerate(documentClustering) if x == j]
-        for j in range(len(set(documentClustering)))
+    document_clusters = [
+        [index_to_document[i]['text']
+         for (i, x) in enumerate(document_clustering) if x == j]
+        for j in range(len(set(document_clustering)))
     ]
+
+    return word_clusters, document_clusters
+
+if __name__ == "__main__":
+    word_clusters, document_clusters = cluster_stories(load())
