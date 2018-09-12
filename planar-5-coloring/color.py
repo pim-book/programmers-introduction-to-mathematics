a26f9ca40fdde6283ddd07fb32b8c84f6617fdcd
Jeremy Kun <j2kun@users.noreply.github.com>
2018-09-11 18:24:28 -0700

    planar-5-coloring: convert to underscore case and add tests

--- planar-5-coloring/color.py
+++ planar-5-coloring/color.py
@@ -1,9 +1,8 @@
 from itertools import combinations
 
 
-def makeMergeMap(G, w1, w2):
-    '''
-      Make a list to denote the merging of two vertices the list "maps" the indices
+def make_merge_map(G, w1, w2):
+    ''' Make a list to denote the merging of two vertices the list "maps" the indices
     of vertices in the unmerged graph to the indices of the vertices in the merged
     graph.  For example, merging vertices 2 and 4 might consist of the list
     [1,2,0,3,0,4,5] or [0,1,2,3,2,4,5], i.e. index i of the list is the "old index"
@@ -11,20 +10,20 @@ def makeMergeMap(G, w1, w2):
     choose, just which vertices are mapped to the same index.
     '''
     n = len(G.vs)
-    noOffset = list(range(w2.index))
-    offsetPart = [w1.index] + [w2.index + j for j in range(n - w2.index - 1)]
+    no_offset = list(range(w2.index))
+    offset_part = [w1.index] + [w2.index + j for j in range(n - w2.index - 1)]
 
-    return noOffset + offsetPart
+    return no_offset + offset_part
 
 
-def mergeTwo(G, w1, w2):
+def merge_two(G, w1, w2):
     # merge two vertices by constructing a merge map for igraph's
     # contract_vertices function
-    mergeMap = makeMergeMap(G, w1, w2)
-    G.contract_vertices(mergeMap, combine_attrs=(lambda x: x))
+    merge_map = make_merge_map(G, w1, w2)
+    G.contract_vertices(merge_map, combine_attrs=(lambda x: x))
 
 
-def findTwoNonadjacent(G, nodes):
+def find_two_nonadjacent(G, nodes):
     for x, y in combinations(nodes, 2):
         if not G.are_connected(x, y):
             return x, y
@@ -33,7 +32,7 @@ def findTwoNonadjacent(G, nodes):
 colors = list(range(5))
 
 
-def planarFiveColor(G):
+def planar_five_color(G):
     # Color a planar graph with five colors by setting the 'color'
     # attribute of the nodes of the input graph.
     # planarFiveColor: Graph -> Graph
@@ -43,33 +42,33 @@ def planarFiveColor(G):
         G.vs['color'] = colors[:n]
         return G
 
-    degAtMost5Nodes = G.vs.select(_degree_le=5)
-    degAtMost4Nodes = degAtMost5Nodes.select(_degree_le=4)
-    deg5Nodes = degAtMost5Nodes.select(_degree_eq=5)
+    deg_at_most5_nodes = G.vs.select(_degree_le=5)
+    deg_at_most4_nodes = deg_at_most5_nodes.select(_degree_le=4)
+    deg5_nodes = deg_at_most5_nodes.select(_degree_eq=5)
 
-    GPrime = G.copy()
-    GPrime.vs['oldIndex'] = list(range(n))  # preserved when deleting vertices
+    g_prime = G.copy()
+    g_prime.vs['old_index'] = list(range(n))  # preserved when deleting vertices
 
-    if len(degAtMost4Nodes) > 0:
-        v = degAtMost4Nodes[0]
-        GPrime.delete_vertices(v.index)
+    if len(deg_at_most4_nodes) > 0:
+        v = deg_at_most4_nodes[0]
+        g_prime.delete_vertices(v.index)
     else:
-        v = deg5Nodes[0]
-        nbrIndices = [x['oldIndex'] for x in GPrime.vs[v.index].neighbors()]
+        v = deg5_nodes[0]
+        nbr_indices = [x['old_index'] for x in g_prime.vs[v.index].neighbors()]
 
-        GPrime.delete_vertices(v.index)
-        neighborsInGPrime = GPrime.vs.select(oldIndex_in=nbrIndices)
+        g_prime.delete_vertices(v.index)
+        neighbors_in_g_prime = g_prime.vs.select(old_index_in=nbr_indices)
 
-        w1, w2 = findTwoNonadjacent(GPrime, neighborsInGPrime)
-        mergeTwo(GPrime, w1, w2)
+        w1, w2 = find_two_nonadjacent(g_prime, neighbors_in_g_prime)
+        merge_two(g_prime, w1, w2)
 
-    coloredGPrime = planarFiveColor(GPrime)
+    colored_g_prime = planar_five_color(g_prime)
 
-    for w in coloredGPrime.vs:
+    for w in colored_g_prime.vs:
         # subset selection handles the merged w1, w2
-        G.vs[w['oldIndex']]['color'] = w['color']
+        G.vs[w['old_index']]['color'] = w['color']
 
-    neighborColors = set(w['color'] for w in v.neighbors())
-    v['color'] = [j for j in colors if j not in neighborColors][0]
+    neighbor_colors = set(w['color'] for w in v.neighbors())
+    v['color'] = [j for j in colors if j not in neighbor_colors][0]
 
     return G
---
+++ planar-5-coloring/color_test.py
@@ -0,0 +1,61 @@
+from assertpy import assert_that
+import igraph
+
+from color import planar_five_color
+from color import make_merge_map
+
+
+def improperly_colored(G, e):
+    return G.vs[e.source]['color'] == G.vs[e.target]['color']
+
+
+def properly_colored(G):
+    return 0 == sum(1 for e in G.es if improperly_colored(G, e))
+
+
+def test_five_node_empty_graph():
+    G = igraph.Graph(n=5)
+    planar_five_color(G)
+    assert_that(G.vs['color']).is_equal_to([0, 1, 2, 3, 4])
+
+
+def test_three_node_empty_graph():
+    G = igraph.Graph(n=3)
+    planar_five_color(G)
+    assert_that(G.vs['color']).is_equal_to([0, 1, 2])
+
+
+def test_color_with_degree_four_node():
+    G = igraph.Graph.Full(n=6)
+    G.delete_edges([(0, 1)])
+    assert_that(G.degree(0)).is_equal_to(4)
+
+    planar_five_color(G)
+    assert_that(properly_colored(G)).is_true()
+
+
+def test_merge_map():
+    G = igraph.Graph.Full(n=6)
+    w1 = G.vs[1]
+    w2 = G.vs[3]
+    actual_merged_map = make_merge_map(G, w1, w2)
+    assert_that(actual_merged_map).is_equal_to([0, 1, 2, 1, 3, 4])
+
+
+def test_color_five_regular_planar_graph():
+    G = igraph.Graph(n=12)
+    flower = [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5),
+              (1, 2), (1, 5), (1, 6), (1, 7),
+              (2, 3), (2, 7), (2, 8),
+              (3, 4), (3, 8), (3, 9),
+              (4, 5), (4, 9), (4, 10),
+              (5, 6), (5, 10),
+              (6, 7), (6, 10), (6, 11),
+              (7, 8), (7, 11),
+              (8, 9), (8, 11),
+              (9, 10), (9, 11),
+              (10, 11)]
+
+    G.add_edges(flower)
+    planar_five_color(G)
+    assert_that(properly_colored(G)).is_true()
