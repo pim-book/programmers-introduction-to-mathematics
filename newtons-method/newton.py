d898fa0ed6760858ea97aad3a0a9de73919813e4
Jeremy Kun <j2kun@users.noreply.github.com>
2018-09-13 18:22:28 -0700

    newtons-method: docstring for main function

--- newtons-method/newton.py
+++ newtons-method/newton.py
@@ -1,5 +1,20 @@
 
 def newton_sequence(f, f_derivative,  starting_x):
+    """Perform Newton's method to find the root of a differentiable funciton.
+
+    Arguments:
+        f: a callable specifying the function whose root is sought.
+        f_derivative: a callable providing the derivative of f at any
+        given input.
+        starting_x: the starting guess for the root. If this value is too
+        far from the true root (where "too far" depends on how chaotic
+        f is near the root) then Newton's method may fail to converge to
+        the root.
+
+    Returns:
+        A generator that yields the x values corresponding to
+        steps of Newton's method.
+    """
     x = starting_x
     while True:
         yield x
