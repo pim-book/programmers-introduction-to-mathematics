68451162c2f0df2dcea5d900adfdda21cd1f9c27
Jeremy Kun <j2kun@users.noreply.github.com>
2018-10-14 11:59:40 -0700

    planar-5-coloring: clean up color.py

--- planar-5-coloring/color.py
+++ planar-5-coloring/color.py
@@ -1,53 +1,49 @@
 from itertools import combinations
 
 
-def make_merge_map(G, w1, w2):
-    ''' Make a list to denote the merging of two vertices the list "maps" the indices
-    of vertices in the unmerged graph to the indices of the vertices in the merged
-    graph.  For example, merging vertices 2 and 4 might consist of the list
-    [1,2,0,3,0,4,5] or [0,1,2,3,2,4,5], i.e. index i of the list is the "old index"
-    and the value is the new index, but it doesn't matter which new indices you
-    choose, just which vertices are mapped to the same index.
-    '''
-    n = len(G.vs)
-    no_offset = list(range(w2.index))
-    offset_part = [w1.index] + [w2.index + j for j in range(n - w2.index - 1)]
-
-    return no_offset + offset_part
-
-
-def merge_two(G, w1, w2):
-    # merge two vertices by constructing a merge map for igraph's
-    # contract_vertices function
-    merge_map = make_merge_map(G, w1, w2)
-    G.contract_vertices(merge_map, combine_attrs=(lambda x: x))
-
-
-def find_two_nonadjacent(G, nodes):
-    for x, y in combinations(nodes, 2):
-        if not G.are_connected(x, y):
-            return x, y
-
-
 colors = list(range(5))
 
 
-def planar_five_color(G):
-    # Color a planar graph with five colors by setting the 'color'
-    # attribute of the nodes of the input graph.
-    # planarFiveColor: Graph -> Graph
-    n = len(G.vs)
+class NotPlanarError(Exception):
+    pass
+
+
+def find_two_nonadjacent(graph, nodes):
+    """ Return two vertices from nodes that are not adjacent. """
+    for x, y in combinations(nodes, 2):
+        if not graph.are_connected(x, y):
+            return x, y
+
+
+def planar_five_color(graph):
+    """ Color a planar graph with five colors.
+
+    The output is produced by setting the 'color' attribute of graph.vs to be
+    integers between 0 and 4.
+
+    Arguments:
+      graph: an igraph Graph object to color.
+
+    Returns:
+      the input graph with its 'color' attribute modified.
+    """
+    n = len(graph.vs)
 
     if n <= 5:
-        G.vs['color'] = colors[:n]
-        return G
+        graph.vs['color'] = colors[:n]
+        return graph
 
-    deg_at_most5_nodes = G.vs.select(_degree_le=5)
+    deg_at_most5_nodes = graph.vs.select(_degree_le=5)
     deg_at_most4_nodes = deg_at_most5_nodes.select(_degree_le=4)
     deg5_nodes = deg_at_most5_nodes.select(_degree_eq=5)
 
-    g_prime = G.copy()
-    g_prime.vs['old_index'] = list(range(n))  # preserved when deleting vertices
+    if not deg5_nodes:
+        raise ValueError("Input graph (or recursive subgraph) does not "
+            "have a degree 5 node. Input graph is not planar.")
+
+    g_prime = graph.copy()
+    # preserved when deleting vertices
+    g_prime.vs['old_index'] = list(range(n))
 
     if len(deg_at_most4_nodes) > 0:
         v = deg_at_most4_nodes[0]
@@ -59,16 +55,48 @@ def planar_five_color(G):
         g_prime.delete_vertices(v.index)
         neighbors_in_g_prime = g_prime.vs.select(old_index_in=nbr_indices)
 
-        w1, w2 = find_two_nonadjacent(g_prime, neighbors_in_g_prime)
+        result = find_two_nonadjacent(g_prime, neighbors_in_g_prime)
+
+        if not result:
+            raise NotPlanarError("Unable to find two nonadjacent vertices "
+                "for recursive call. Input graph is not planar.")
+        w1, w2 = result
         merge_two(g_prime, w1, w2)
 
     colored_g_prime = planar_five_color(g_prime)
 
     for w in colored_g_prime.vs:
         # subset selection handles the merged w1, w2
-        G.vs[w['old_index']]['color'] = w['color']
+        graph.vs[w['old_index']]['color'] = w['color']
 
     neighbor_colors = set(w['color'] for w in v.neighbors())
     v['color'] = [j for j in colors if j not in neighbor_colors][0]
 
-    return G
+    return graph
+
+
+def make_merge_map(G, w1, w2):
+    """ Make a list to denote the merging of two vertices.
+
+    The list "maps" the indices of vertices in the unmerged graph to the
+    indices of the vertices in the merged graph.
+
+    For example, merging vertices 2 and 4 might consist of the list
+    [1, 2, 0, 3, 0, 4, 5] or [0, 1, 2, 3, 2, 4, 5], i.e., index i of the
+    list is the "old index" and the value is the new index, but it doesn't
+    matter which new indices you choose, just which vertices are mapped to
+    the same index.
+    """
+    n = len(G.vs)
+    no_offset = list(range(w2.index))
+    offset_part = [w1.index] + [w2.index + j for j in range(n - w2.index - 1)]
+    return no_offset + offset_part
+
+
+def merge_two(graph, w1, w2):
+    """ Merge the two input vertices in the input graph.
+
+    Note this mutates the input graph, so the caller is responsible for making a copy.
+    """
+    merge_map = make_merge_map(graph, w1, w2)
+    graph.contract_vertices(merge_map, combine_attrs=(lambda x: x))
