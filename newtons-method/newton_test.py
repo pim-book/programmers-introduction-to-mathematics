1fd4ae74c200cbb6d252ff90e841229df9c4ff87
Jeremy Kun <j2kun@users.noreply.github.com>
2018-10-27 23:00:14 -0700

    newtons-method: pep8

--- newtons-method/newton_test.py
+++ newtons-method/newton_test.py
@@ -7,14 +7,19 @@ EPSILON = 1e-4
 
 
 def test_newtons_sequence_converge():
-    def f(x): return x**5 - x - 1
-    def f_derivative(x): return 5 * x**4 - 1
+    def f(x):
+        return x**5 - x - 1
+
+    def f_derivative(x):
+        return 5 * x**4 - 1
+
     starting_x = 1
 
     approximation = [
-        (x, f(x))
-        for (i, x) in takewhile(lambda z: z[0] < 10,
-            enumerate(newton_sequence(f, f_derivative, starting_x)))
+        (x, f(x)) for (i, x) in takewhile(
+            lambda z: z[0] < 10,
+            enumerate(newton_sequence(f, f_derivative, starting_x))
+        )
     ]
 
     assert_that(approximation[-1][0]).is_close_to(1.1673, EPSILON)
@@ -22,14 +27,18 @@ def test_newtons_sequence_converge():
 
 
 def test_newtons_sequence_fails_to_converge():
-    def f(x): return x**5 - x - 1
-    def f_derivative(x): return 5 * x**4 - 1
-    starting_x = 0
+    def f(x):
+        return x**5 - x - 1
 
+    def f_derivative(x):
+        return 5 * x**4 - 1
+
+    starting_x = 0
     approximation = [
-        (x, f(x))
-        for (i, x) in takewhile(lambda z: z[0] < 10000,
-            enumerate(newton_sequence(f, f_derivative, starting_x)))
+        (x, f(x)) for (i, x) in takewhile(
+            lambda z: z[0] < 10000,
+            enumerate(newton_sequence(f, f_derivative, starting_x))
+        )
     ]
 
     for (x, y) in approximation:
